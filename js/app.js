c9556247279856daf668779a71e6777b8500129a
Zahra Alvandi <zalvandi39@gmail.com>
2025-05-17 22:51:33 +0330

    key-logger

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const keyElem = document.querySelector(".key");
+const locationElem = document.querySelector(".location");
+const codeElem = document.querySelector(".code");
+const whichElem = document.querySelector(".which");
+const keyCodeTitle = document.querySelector(".keycode-title");
+const keyCodeElem = document.querySelector(".keycode");
+
+document.body.addEventListener("keydown", function (event) {
+    event.preventDefault();
+
+    // document.body.className = "key-pressed";
+    document.body.classList.add("key-pressed");
+
+    keyElem.innerHTML = event.key;
+    locationElem.innerHTML = event.location;
+    codeElem.innerHTML = event.code;
+    whichElem.innerHTML = event.which;
+    keyCodeTitle.innerHTML = event.which;
+    keyCodeElem.innerHTML = event.key;
+});
