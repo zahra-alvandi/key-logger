b1cddd83434b911744e9e5c16292edf4df26e5c1
Zahra Alvandi <zalvandi39@gmail.com>
2025-05-18 10:20:56 +0330

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
-const keyElem = document.querySelector(".key");
+// const keyElem = document.querySelector(".key");
+// const locationElem = document.querySelector(".location");
+// const codeElem = document.querySelector(".code");
+// const whichElem = document.querySelector(".which");
+// const keyCodeTitle = document.querySelector(".keycode-title");
+// const keyCodeElem = document.querySelector(".keycode");
+
+// document.body.addEventListener("keydown", function (event) {
+//     event.preventDefault();
+
+//     // document.body.className = "key-pressed";
+//     document.body.classList.add("key-pressed");
+
+//     keyElem.innerHTML = event.key;
+//     locationElem.innerHTML = event.location;
+//     codeElem.innerHTML = event.code;
+//     whichElem.innerHTML = event.which;
+//     keyCodeTitle.innerHTML = event.which;
+//     keyCodeElem.innerHTML = event.key;
+// });
+
+const keyCodeElem = document.querySelector(".keycode");
+const keyCodeTitle = document.querySelector(".keycode-title");
 const locationElem = document.querySelector(".location");
 const codeElem = document.querySelector(".code");
 const whichElem = document.querySelector(".which");
-const keyCodeTitle = document.querySelector(".keycode-title");
-const keyCodeElem = document.querySelector(".keycode");
+const keyElem = document.querySelector(".key");
 
 document.body.addEventListener("keydown", function (event) {
     event.preventDefault();
 
-    // document.body.className = "key-pressed";
     document.body.classList.add("key-pressed");
 
     keyElem.innerHTML = event.key;
     locationElem.innerHTML = event.location;
+    keyCodeElem.innerHTML = event.key;
+    keyCodeTitle.innerHTML = event.which;
     codeElem.innerHTML = event.code;
     whichElem.innerHTML = event.which;
-    keyCodeTitle.innerHTML = event.which;
-    keyCodeElem.innerHTML = event.key;
 });
